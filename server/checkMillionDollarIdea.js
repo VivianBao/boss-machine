fbee5991a958da9f7987fc3088b5763f13b6012a
Vivian Bow <vivianbao1009@gmail.com>
2023-11-07 20:26:56 +0800

    Built checkMillionDollar middleware function

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,12 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+  const numWeeks = Number(req.body.numWeeks);
+  const weeklyRevenue = Number(req.body.weeklyRevenue);
+  const sum = numWeeks*weeklyRevenue
+  if(!sum >= 1000000){
+    res.status(400).send("This idea is not worth 1 million dollars")
+  }
+  next();
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
